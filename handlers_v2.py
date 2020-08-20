dd1c2aa34cfed44fe7ababe28f306bcaa2f73d6e
dcflachs <dcflachs@gmail.com>
2020-08-19 21:38:48 -0400

    Added NodeV2WriteHandler

--- handlers_v2.py
+++ handlers_v2.py
@@ -42,6 +42,7 @@ from handlers import NodeBaseHandler
 class V2HandlersState():
     def __init__(self, cur, conns):
         self.well_known = {}
+        self.write_cache = {}
         self.v2_listener = V2Listener(self, cur, conns)
 
 
@@ -54,13 +55,17 @@ class V2Listener():
         rows = cur.fetchall()
         for row in rows:
             self.well_known_subscriber(row["user_id"])
+            self.queued_write_subscriber(row["user_id"])
 
     @gen.coroutine
     def well_known_subscriber(self, user_id):
         event_q = CoEventBus().listener('/event/users/{}'.format(user_id)).create_queue()
         while True:
             event = yield event_q.get()
-            if event and event["event_type"] == "stat" and event["event_data"]["online"]:
+            if (event and 
+                event["event_type"] == "stat" and 
+                event["event_data"]["online"] and 
+                event["event_data"]["at"] == "xchange"):
                 node_sn = event["node_sn"]
                 self.db_cur.execute("select * from nodes where node_sn='%s'" % (node_sn))
                 r = self.db_cur.fetchone()
@@ -71,9 +76,9 @@ class V2Listener():
 
     @gen.coroutine
     def attempt_request(self, uri, node_sn, conns, name):
+        result_ok = False
         if node_sn in conns:
             conn = conns[node_sn]
-            result_ok = False
             for x in range(3):
                 if not conn.killed:
                     try:
@@ -91,6 +96,49 @@ class V2Listener():
                     except web.HTTPError:
                         pass
                     except Exception, e:
+                        gen_log.error(e)
+        raise gen.Return(result_ok)
+
+    @gen.coroutine
+    def queued_write_subscriber(self, user_id):
+        event_q = CoEventBus().listener('/event/users/{}'.format(user_id)).create_queue()
+        while True:
+            event = yield event_q.get()
+            if (event and 
+                event["event_type"] == "stat" and 
+                event["event_data"]["online"] and 
+                event["event_data"]["at"] == "xchange"):
+                node_sn = event["node_sn"]
+                if node_sn in self.v2_state.write_cache:
+                    self.db_cur.execute("select * from nodes where node_sn='%s'" % (node_sn))
+                    r = self.db_cur.fetchone()
+                    if r:
+                        keys = [k for k in self.v2_state.write_cache[node_sn]]
+                        yield gen.sleep(0.5)
+                        for key in keys:
+                            uri = self.v2_state.write_cache[node_sn][key]
+                            del self.v2_state.write_cache[node_sn][key]
+                            gen_log.info('INFO: V2 {}'.format(uri))
+                            response = yield self.post(uri, node_sn, self.conns)
+
+    @gen.coroutine
+    def post (self, uri, node_sn, conns):
+        result_ok = False
+        if node_sn in self.conns:
+            conn = conns[node_sn]
+            for x in range(10):
+                if not conn.killed:
+                    try:
+                        ok, resp = yield conn.submit_and_wait_resp(uri, "resp_post", 1)
+                        if not ('status' in resp and resp['status'] != 200):
+                            result_ok = True
+                            break
+                        if 'status' in resp and resp['status'] == 404:
+                            gen_log.info('INFO: V2 post to {} failed 404'.format(uri))
+                            break
+                    except web.HTTPError:
+                        pass
+                    except Exception,e:
                         gen_log.error(e)
         raise gen.Return(result_ok)
 
@@ -155,3 +203,88 @@ class NodeV2WellKnownHandler(NodeBaseHandler):
                 except Exception, e:
                     gen_log.error(e)
         raise gen.Return(result_ok)
+
+class NodeV2WriteHandler(NodeBaseHandler):
+
+    def initialize (self, conns, state_waiters, state_happened, state_cached):
+        self.conns = conns
+        self.state_waiters = state_waiters
+        self.state_happened = state_happened
+        self.write_cache = state_cached.write_cache
+
+    @gen.coroutine
+    def pre_request(self, req_type, uri):
+        return True
+
+    @gen.coroutine
+    def post_request(self, req_type, uri, resp):
+        pass
+
+    @gen.coroutine
+    def post (self, uri):
+
+        uri = uri.split("?")[0].rstrip("/")
+        gen_log.info("INFO: V2 POST to: "+ str(uri))
+
+        uri_parts = uri.split("/")
+
+        node = self.get_node()
+        if not node:
+            return
+        self.node = node
+        node_sn = node['node_sn']
+
+        if uri_parts[0] == ".clear":
+            base_uri ="/".join((uri_parts[1:]))
+            if ( node_sn in self.write_cache and 
+                 base_uri in self.write_cache[node_sn] ):
+                del self.write_cache[node_sn][base_uri]
+                self.resp(200,meta="Cleared")
+            else:
+                self.resp(400,meta="No Queued Command Found")
+
+            return
+
+        if self.request.headers.get("content-type") and self.request.headers.get("content-type").find("json") > 0:
+            self.resp(400, "Can not accept application/json post request.")
+            return
+
+        if not self.pre_request('post', uri):
+            return
+
+        cmd = "POST /%s\r\n"%(uri)
+        cmd = cmd.encode("ascii")
+
+        #Post the request directly
+        if node_sn in self.conns:
+            conn = self.conns[node_sn]
+            if not conn.killed:
+                try:
+                    ok, resp = yield conn.submit_and_wait_resp(cmd, "resp_post")
+                    if 'status' in resp and resp['status'] == 404:
+                        self.resp(404,meta=resp['msg'])
+                        return
+                    elif 'status' in resp and resp['status'] != 200:
+                        pass
+                    else:
+                        self.resp(200,meta=resp['msg'])
+                        gen_log.info('INFO: V2 POST Command Sent Directly')
+                        return
+                except web.HTTPError:
+                    pass
+                except Exception,e:
+                    gen_log.error(e)
+        
+        #Queue the post for later.
+        if not node_sn in self.write_cache:
+            self.write_cache[node_sn] = {}
+        
+        base_uri ="/".join((uri_parts[0:-1]))
+
+        msg = "Command Queued"
+        if base_uri in self.write_cache[node_sn]:
+            msg = "Command Overwritten"
+
+        self.write_cache[node_sn][base_uri] = cmd
+        gen_log.info('INFO: V2 POST {}'.format(msg))
+        self.resp(200, msg)
